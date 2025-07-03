757430d91102785750670a41ae7c12137218b6e2
Minahil Zahra <minahilzahrachatha@gmail.com>
2025-07-03 12:45:40 +0500

    Add files via upload

---
+++ MethodsOfPrimitive.js
@@ -0,0 +1,19 @@
+let john = {
+    name: "John",
+    sayHi: function() {
+      alert("Hi buddy!");
+    }
+  };
+  
+  john.sayHi();
+
+  let str = "Hello";
+
+alert( str.toUpperCase() );
+
+let zero = new Number(0);
+
+if (zero) { 
+  alert( "zero is truthy!?!" );
+}
+
